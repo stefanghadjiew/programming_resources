28f9a0084e2bdf5b5bc90714ec45f4feecd55d15
Stefan <shadzhiev@eservicios.indra.es>
2025-08-09 22:19:14 +0300

    moove Logout button tailwind styles to classes object and remove clsx from Navbar

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,18 +1,18 @@
 'use client';
 
 import { signUserOut, useSession } from '@lib';
-import { clsx } from 'clsx';
 import { useRouter } from 'next/navigation';
 
 import { Icons } from '@components';
 
 const classes = {
-  navbarWrapper: clsx('m-w-[1400px] m-auto py-[10px] mb-[100px]'),
-  navbar: clsx('py-[10px] flex justify-between'),
-  logo: clsx('text-gray-500'),
+  navbarWrapper: 'm-w-[1400px] m-auto py-[10px] mb-[100px]',
+  navbar: 'py-[10px] flex justify-between',
+  logo: 'text-gray-500',
   navbarItems: 'flex gap-2 items-center',
   navbarItem:
-    ' text-amber-400 opacity-65 hover:opacity-100 transition-opacity duration-200 font-bold px-1 cursor-pointer'
+    ' text-amber-400 opacity-65 hover:opacity-100 transition-opacity duration-200 font-bold px-1 cursor-pointer',
+  logoutButton: 'bg-gray-500 px-2 py-1 rounded text-amber-400 font-bold cursor-pointer'
 };
 
 export const Navbar = () => {
@@ -30,10 +30,7 @@ export const Navbar = () => {
           <li className={classes.navbarItem}>Item 2</li>
           <li className={classes.navbarItem}>Item 3</li>
           {data?.session && (
-            <button
-              onClick={() => signUserOut(() => router.push('/login'))}
-              className="bg-gray-500 px-2 py-1 rounded text-amber-400 font-bold cursor-pointer"
-            >
+            <button onClick={() => signUserOut(() => router.push('/login'))} className={classes.logoutButton}>
               Logout
             </button>
           )}
