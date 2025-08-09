d6de9b9a42371279dffc719d7ab3477b19aa6ce6
Stefan <shadzhiev@eservicios.indra.es>
2025-08-09 18:33:33 +0300

    remove duplicate relative from tailwind class and moove SIDEBAR_LINKS to separate file

--- src/components/ui/Sidebar/Sidebar.tsx
+++ src/components/ui/Sidebar/Sidebar.tsx
@@ -1,41 +1,10 @@
 import { clsx } from 'clsx';
 
-export const SIDEBAR_LINKS = {
-  PROGRAMMING: {
-    REACT: 'React',
-    JAVASCRIPT: 'Javascript',
-    HTML: 'HTML',
-    CSS: 'CSS',
-    NEXTJS: 'NextJS',
-    NODEJS: 'NodeJS',
-    TYPESCRIPT: 'Typescript',
-    DOCKER: 'Docker',
-    BUILDTOOLS: 'Buildtools',
-    GIT: 'Git',
-    DEPLOYMENTS: 'Deployments',
-    LIBRARIES: 'Libraries',
-    ARTICLES: 'Articles',
-    CICD: 'CI / CD'
-  },
-  BOOKS: {
-    ADVENTURE: 'Adventure',
-    PHYSICS: 'Physics',
-    MATHEMATICS: 'Mathematics'
-  },
-  OTHER: {
-    TEST: 'Other 1',
-    TEST2: 'Other 2',
-    TEST3: 'Other 3',
-    TEST4: 'Other 4',
-    TEST5: 'Other 5',
-    TEST6: 'Other 6',
-    TEST7: 'Other 7'
-  }
-} as const;
+import { SIDEBAR_LINKS } from './constants';
 
 const classes = {
   sidebarWrapper: clsx(
-    'relative bg-[080808] h-[calc(100dvh-77px)] flex flex-col relative text-gray-500 min-w-[200px] w-[230px] overflow-y-auto custom-scrollbar'
+    'relative bg-[#080808] h-[calc(100dvh-77px)] flex flex-col text-gray-500 min-w-[200px] w-[230px] overflow-y-auto custom-scrollbar'
   ),
   sidebarLinksWrapper: clsx('text-amber-400 font-bold flex flex-col gap-2'),
   sidebarLinksWrapperList: clsx('flex flex-col gap-2 pl-[24px] border-l-2 border-gray-500'),
---
+++ src/components/ui/Sidebar/constants.ts
@@ -0,0 +1,32 @@
+export const SIDEBAR_LINKS = {
+  PROGRAMMING: {
+    REACT: 'React',
+    JAVASCRIPT: 'Javascript',
+    HTML: 'HTML',
+    CSS: 'CSS',
+    NEXTJS: 'NextJS',
+    NODEJS: 'NodeJS',
+    TYPESCRIPT: 'Typescript',
+    DOCKER: 'Docker',
+    BUILDTOOLS: 'Buildtools',
+    GIT: 'Git',
+    DEPLOYMENTS: 'Deployments',
+    LIBRARIES: 'Libraries',
+    ARTICLES: 'Articles',
+    CICD: 'CI / CD'
+  },
+  BOOKS: {
+    ADVENTURE: 'Adventure',
+    PHYSICS: 'Physics',
+    MATHEMATICS: 'Mathematics'
+  },
+  OTHER: {
+    TEST: 'Other 1',
+    TEST2: 'Other 2',
+    TEST3: 'Other 3',
+    TEST4: 'Other 4',
+    TEST5: 'Other 5',
+    TEST6: 'Other 6',
+    TEST7: 'Other 7'
+  }
+} as const;
